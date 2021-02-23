95cdd3921b3b25e278f170b0b0294d6272d9ca14
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2021-02-23 18:54:14 +0100

    No longer draws grid. Green indicator shows center and puts cone at origin, this can be disable by removing the line f.write(p 0.0 0.0 0.0). It still doesn't do very well with sharp corners

--- DrawTrack.py
+++ DrawTrack.py
@@ -2,26 +2,17 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-im_size = 720 * 3
+im_size = 800
+area_size_m = 80 #The area is a area_size_m x area_size_m area. Therefore each pixel represents im_size / area_size_m meters
 blank_image = np.zeros((im_size, im_size, 3), np.uint8)
-num_grids = 720 #Ideally this must be an integer factor of im_size
-grid_size = int(im_size / num_grids)
 blank_image[:] = (255, 255, 255)
 
-blank_image[0:im_size:grid_size, :] = (0, 0, 0)
-blank_image[:, 0:im_size:grid_size] = (0, 0, 0)
-
-
-grid_representation = 2.5 #Physical distance representation between the centre points
-# of two adjacent grid spaces in meters.
-grid_center_points = np.zeros(num_grids - 1)
-for i in range(num_grids - 1):
-    grid_center_points[i] = grid_size / 2 + i * grid_size
+indicator_radius_px = 5 #radius of indicator size when clicking. 
 
 track_directory = "/mnt/c/Users/Rufus Vijayaratnam/Documents/University/Year 3/IP/Blender/Resources/Tracks/"
-track_name = "track5.txt"
+track_name = "track2.txt"
 
-
+#This is unencessary but I don't want to remove it because of the file reading.
 isClosed = False
 f = open(track_directory+track_name, 'w+')
 f.write("Track\n")
@@ -31,34 +22,26 @@ else:
     f.write("open\n")
 
 #Lines below sets 0.0 0.0 0.0 origin point
-p1 = int(im_size / 2 - int(grid_size / 2))
-p2 = int(im_size / 2 + int(grid_size / 2))
-blank_image[p1:p2, p1:p2] = (0, 0, 0)
+center_point = (int(im_size / 2), int(im_size / 2))
+cv.circle(blank_image, center_point, indicator_radius_px, (0, 255, 0), -1)
 
 f.write("p 0.0 0.0 0.0\n")
 
 def write_point(file, point_x, point_y):
     point_x -= float(im_size / 2)
     point_y -= float(im_size / 2)
-    point_x = point_x * grid_representation / grid_size * -1 #Just because
-    point_y = point_y * grid_representation / grid_size
+    """ point_x = point_x * grid_representation / grid_size * -1 #Just because
+    point_y = point_y * grid_representation / grid_size """
+    point_x /= (im_size / area_size_m)
+    point_y /= (im_size / area_size_m)
     f.write("p %f %f 0.0 \r\n" % (point_x, point_y))
 
 
 def add_point(event, x, y, flags, params):
     if event == cv.EVENT_LBUTTONUP:
-        x_idx = (np.abs(grid_center_points - x)).argmin()
-        y_idx = (np.abs(grid_center_points - y)).argmin()
-        point_x = grid_center_points[x_idx]
-        point_y = grid_center_points[y_idx]
-        p1x = int(point_x - grid_size / 2)
-        p1y = int(point_y - grid_size / 2)
-        p2x = int(point_x + grid_size / 2)
-        p2y = int(point_y + grid_size / 2)
-        blank_image[p1y:p2y, p1x:p2x] = (0, 0, 0)
-        write_point(f, point_x, point_y)
+        cv.circle(blank_image, (x, y), indicator_radius_px, (0, 0, 0), -1)
+        write_point(f, x, y)
         cv.imshow("blank image", blank_image)
-
 
 cv.imshow("blank image", blank_image)
 cv.setMouseCallback("blank image", add_point)
