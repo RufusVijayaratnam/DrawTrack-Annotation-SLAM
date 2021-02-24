0c8fa61714eaaa9cb0d1fcef8419e6f16eb48580
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2021-02-24 17:31:51 +0100

    This is a fucking mess

--- LoadTrack.py
+++ LoadTrack.py
@@ -62,28 +62,20 @@ def stereo_cam_ext(filepath, substeps=2, cam_spacing=0.5):
     step_count = 0
     up_unit_vec = [0, 0, 1]
     forward_unit = np.array([0, -1, 0])
-    for i in range(render_pairs - 1):
+    for i in range(2):
         p1 = track_points_ws[i + 1]
         p0 = track_points_ws[i]
-        d1 = track_direction[i + 1]
-        d0 = track_direction[i]
-        print("d0: \n", d0)
         total_turn_angle_rad = np.arccos(np.dot(d1, d0)) #About Z blender space
         print("turn angle is %f degrees" % np.rad2deg(total_turn_angle_rad))
         step_turn_angle_rad = total_turn_angle_rad / substeps
         vec = np.subtract(p1, p0)
         step_vec = vec / substeps
         for j in range(substeps):
-            rotation_matrix = gen_rotation_matrix(0, 0, -step_turn_angle_rad * (j + 1))
-            print("current turn angle is: ", np.rad2deg((step_turn_angle_rad * (j + 1))))
-            direction = rotation_matrix * np.matrix(d0).transpose()
-            direction = np.asarray(direction.transpose())[0]
-            cone_chord = np.cross(up_unit_vec, direction)
-            print("cone_chord: \n", cone_chord)
             point = p0 + step_vec * j
-            right_cam_loc_ws = point - 0.5 * cam_spacing * cone_chord 
+            cam_chord = np.cross(up_unit_vec, direction)
+            right_cam_loc_ws = point - 0.5 * cam_spacing * cam_chord 
             right_cam_loc_ws[2] = 0.5
-            left_cam_loc_ws = point + 0.5 * cam_spacing * cone_chord
+            left_cam_loc_ws = point + 0.5 * cam_spacing * cam_chord
             left_cam_loc_ws[2] = 0.5
             rz = 180 - np.rad2deg(np.arccos(np.dot(direction, forward_unit) / (np.linalg.norm(direction))))
             print("rx: ", rz)
@@ -96,4 +88,3 @@ def stereo_cam_ext(filepath, substeps=2, cam_spacing=0.5):
   
     return left_cam_points_ws, right_cam_points_ws, blender_cam_rotation_ws
     
-        
