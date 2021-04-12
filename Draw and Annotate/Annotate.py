926216b482c9ea725943561dc371903b27e5daee
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2021-04-12 11:47:50 +0100

    Removed support for haar cascade annotation, will never use it. Changed annotation program to use Detections and DetectedCone class instead of dumb image points class, code is much simpler now. Added another output file from annotation process that write the physical locations of cones that can be seen in an image, will be useful for debugging.

--- Draw and Annotate/Annotate.py
+++ Draw and Annotate/Annotate.py
@@ -4,44 +4,18 @@ import LoadTrack as lt
 from MatrixTools import *
 import cv2 as cv
 import os
+import sys
+sys.path.append("/mnt/c/Users/Rufus Vijayaratnam/Driverless/Location and Mapping/")
+from Detections import *
+from Colour import *
 
 cone_height_m = 0.262
 cone_widht_m = 0.324
 up_vec = np.array([0, -1, 0]) #CV
 cam_initial_direction = np.array([0, 0, 1]) #CV
-train_or_val = "train"
-
-
-class ImagePoints:
-    
-    def __init__(self, num_cone_points, res_x, res_y):
-        self.blue_points = np.ndarray(num_cone_points, dtype=(tuple, 2))
-        self.yellow_points = np.ndarray(num_cone_points, dtype=(tuple, 2))
-        self.res_x = res_x
-        self.res_y = res_y
-    
-    def on_screen(self, point):
-        if point[0] > 0 and point[1] > 0 and point[0] <= self.res_x and point[1] <= self.res_y:
-            return True
-        else:
-            return False
-    
-    def swap_points(self, arry):
-        arry = np.asarray([((int(p[0][1]), int(p[0][0])), (int(p[1][1]), int(p[1][0]))) for p in arry], dtype=(tuple, 2))
-        return arry
-        
-    def clean(self):
-        blue_points_cleaned = np.asarray([point for point in self.blue_points if self.on_screen(point[0]) and self.on_screen(point[1])])
-        yellow_points_cleaned = np.asarray([point for point in self.yellow_points if self.on_screen(point[0]) and self.on_screen(point[1])])
-        self.blue_points = np.array(blue_points_cleaned)
-        self.yellow_points = np.array(yellow_points_cleaned)
-        #Should get rid of this and make a better way of casting to int
-        self.blue_points = self.swap_points(self.blue_points)
-        self.yellow_points = self.swap_points(self.yellow_points)
-        self.blue_points = self.swap_points(self.blue_points)
-        self.yellow_points = self.swap_points(self.yellow_points)
-
-    
+train_or_val = "test"
+rsrc = "/mnt/c/Users/Rufus Vijayaratnam/Driverless/Blender/Resources/"
+ 
 
 def axis_transform(point):
     #OpenCV cardinal vectors in blender coordinate system
@@ -78,23 +52,31 @@ def cone_annotation_bounds(cam_loc_ws, cone_loc_ws, intrinsic_matrix, rotation_m
     angle_between_direction_and_cone_cam = np.arccos(np.dot(cone_to_cam_vec_unit, cam_direction_unit))
    
     if angle_between_direction_and_cone_cam >= np.pi / 2:
-        return ((-1, -1), (-1, -1))
+        return -1, -1, -1, -1
     
     img_point1 = intrinsic_matrix * rotation_matrix * p1_cs
+    if img_point1[2] == 0:
+        return -1, -1, -1, -1
     img_point1 = normalise_wrt_indx(img_point1)
     img_point2 = intrinsic_matrix * rotation_matrix * p2_cs
+    if img_point2[2] == 0:
+        return -1, -1, -1, -1
     img_point2 = normalise_wrt_indx(img_point2)
 
     u1 = img_point1[0]
     v1 = img_point1[1]
     u2 = img_point2[0]
     v2 = img_point2[1]
+    x = int((u1 + u2) / 2)
+    y = int((v1 + v2) / 2)
+    w = abs(u1 - u2)
+    h = abs(v1 - v2)
 
-    return ((u1, v1), (u2, v2))
+    return x, y, w, h
 
-def annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name, file, res_x=1920, res_y=1080):
+def annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name, res_x=1920, res_y=1080):
     sep = os.sep
-    img_points = ImagePoints(len(blue_cone_ws), res_x, res_y)
+    #img_points = ImagePoints(len(blue_cone_ws), res_x, res_y)
     num_cones = len(blue_cone_ws)
 
     cam_rotation_rad = np.deg2rad(cam_rotation)
@@ -111,62 +93,51 @@ def annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_w
                                 [0, 0, 1]])
 
     cam_direction_unit = rotation_matrix.T * np.matrix(cam_initial_direction).transpose()
+    cones = []
     for i, (blue_cone, yellow_cone) in enumerate(zip(blue_cone_ws, yellow_cone_ws)):
-        blue_point = cone_annotation_bounds(cam_loc_ws, blue_cone, intrinsic_matrix, rotation_matrix)
-        yellow_point = cone_annotation_bounds(cam_loc_ws, yellow_cone, intrinsic_matrix, rotation_matrix)
-        img_points.blue_points[i] = blue_point
-        img_points.yellow_points[i] = yellow_point
+        x, y, w, h = cone_annotation_bounds(cam_loc_ws, blue_cone, intrinsic_matrix, rotation_matrix)
+        cone = DetectedCone(x, y, w, h)
+        cone.colour = blue
+        cone.loc_ws = blue_cone
+        cones.append(cone)
 
-    img_points.clean()
+        x, y, w, h = cone_annotation_bounds(cam_loc_ws, yellow_cone, intrinsic_matrix, rotation_matrix)
+        cone = DetectedCone(x, y, w, h)
+        cone.colour = yellow
+        cone.loc_ws = yellow_cone
+        cones.append(cone)
+
+    
     print("Annotating: %s" % (render))
     image_folder = "Renders" + sep + train_or_val + sep + "images" + sep
     labels_folder = "Renders" + sep + train_or_val + sep + "labels" + sep
     file_path = resource_folder + "Renders" + sep + train_or_val + sep + "Annotated-images" + sep + "%s" % (render.replace(".png", ".jpg"))
     active_image = cv.imread(resource_folder + image_folder + render)
+    annotations = Detections(cones)
+    annotations.image = active_image
+    annotations.init_detections()
+    annotations = annotations.remove_off_screen()
     im_width = np.shape(active_image)[1]
     im_height = np.shape(active_image)[0]
     anno_name = render.replace(".png", ".txt")
 
     f_yolo = open(resource_folder + labels_folder + "%s" % (anno_name), "w+")
+    f_real = open(resource_folder + labels_folder + "%s" % ("Real-" + anno_name), "w+")
 
-    total_cones = len(img_points.blue_points) + len(img_points.yellow_points)
-    if total_cones == 0:
-        file.write("0")
-        return
+    for anno in annotations:
+        cx = anno.cx / im_width
+        cy = anno.cy / im_height
+        w = anno.w / im_width
+        h = anno.h / im_height
+        yolo_string = "0 %f %f %f %f\n" % (cx, cy, w, h)
+        f_yolo.write(yolo_string)
+        x_ws, y_ws, z_ws = anno.loc_ws
+        colour = anno.colour.name
+        f_real.write("%s %f %f %f\n" % (colour, x_ws, y_ws, z_ws))
 
-    file.write("%i  " % total_cones) #Double space
-    count = 0
-    for rects in img_points.blue_points:
-        p1 = rects[0]
-        p2 = rects[1]
-        #For Haar Cascade
-        file.write("%i %i %i %i   " %(p1[0], p1[1], p2[0], p2[1])) 
-        #For Yolo:
-        height = float(abs(p2[1] - p1[1]) / im_height)
-        width = float(abs(p2[0] - p1[0])  / im_width)
-        x = float((p1[0] + p2[0]) / 2) / im_width
-        y = float((p1[1] + p2[1]) / 2) / im_height
-        yolo_string = "0 %f %f %f %f\n" % (x, y, width, height)
-        f_yolo.write(yolo_string)
-        cv.rectangle(active_image, p1, p2, (255, 255, 0))
-        
-    
-    for rects in img_points.yellow_points:
-        p1 = rects[0]
-        p2 = rects[1]
-        #For Haar Cascade
-        file.write("%i %i %i %i   " %(p1[0], p1[1], p2[0], p2[1]))
-        #For Yolo:
-        height = float(abs(p2[1] - p1[1]) / im_height)
-        width = float(abs(p2[0] - p1[0])  / im_width)
-        x = float((p1[0] + p2[0]) / 2) / im_width
-        y = float((p1[1] + p2[1]) / 2) / im_height
-        yolo_string = "0 %f %f %f %f\n" % (x, y, width, height)
-        f_yolo.write(yolo_string)
-        cv.rectangle(active_image, p1, p2, (255, 0, 0))
-        
-    cv.imwrite(file_path, active_image)
+    cv.imwrite(file_path, np.array(annotations.get_annotated_image()))
     f_yolo.close()
+    f_real.close()
         
     return
 
@@ -190,20 +161,15 @@ def annotate_track(resource_folder, track_name, substeps, tov="train"):
     renders = [render for render in renders if "%s-" % track_name in render]
 
     imgWidth = cv.imread(resource_folder + images_folder + renders[0]).shape[1]
-    focalLength_mm = 50 #Should use more realistic values
-    sensorWidth_mm = 36
-    sensorHeight_mm = 24
+    focalLength_mm = 5.5 #Should use more realistic values
+    sensorWidth_mm = 8.5
+    sensorHeight_mm = 4.78
     focalLength_pixels = (focalLength_mm / sensorWidth_mm) * imgWidth
     fx = focalLength_pixels
     fy = fx
 
-
-    f = open(resource_folder + labels_folder + "%s-info.dat" % track_name, "w+")
-
-
     for i in range(len(renders)):
         render = renders[i]
-        f.write(resource_folder + images_folder + resource_folder + "  ")
         left = "%s-Left_Cam" % track_name
         right = "%s-Right_Cam" % track_name
         cam_info = render.split("-Render-") #cam_info[0] = left / right, cam_info[1] = cam index
@@ -212,17 +178,14 @@ def annotate_track(resource_folder, track_name, substeps, tov="train"):
         if cam == left:
             cam_loc_ws = left_cam_points_ws[cam_indx]
             cam_rotation = cam_rotation_ws[cam_indx]
-            annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name, f)
-            f.write("\n")
+            annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name)
         elif cam == right:
             cam_loc_ws = right_cam_points_ws[cam_indx]
             cam_rotation = cam_rotation_ws[cam_indx]
-            annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name, f)
-            f.write("\n")
+            annotate_image(render, cam_loc_ws, cam_rotation, blue_cone_ws, yellow_cone_ws, fx, fy, resource_folder, track_name)
         else:
             print("Couldn't determine which camera, skipping this render.")
         
-    f.close()
 
 
 
